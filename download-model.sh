7530320b2be16340a308a7a54891da331a5e41c2
windowsagent <juanjphc@gmail.com>
2023-04-18 14:07:52 -0600

    Fix something in the path of the downloader

--- download-model.sh
+++ download-model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the model ckpt file exists
-if [ ! -f "/stable_diffusion/modelstore/sd-v1-4.ckpt" ]; then
+if [ ! -f "./stable_diffusion/modelstore/sd-v1-4.ckpt" ]; then
     # Download the model ckpt file
-    wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O /stable_diffusion/modelstore/sd-v1-4.ckpt
+    wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O ./stable_diffusion/modelstore/sd-v1-4.ckpt
 fi
