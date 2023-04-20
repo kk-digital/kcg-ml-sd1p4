4236da6e9eeac498d2eef1667670ff641193891c
Kenje Hofileña <59994329+kenje4090@users.noreply.github.com>
2023-04-20 11:04:08 +0800

    Update download output to ./input/models

--- download-model.sh
+++ download-model.sh
@@ -6,7 +6,7 @@ mkdir ./stable_diffusion/modelstore
 # Check if the model ckpt file exists
 if [ ! -f "./stable_diffusion/modelstore/sd-v1-4.ckpt" ]; then
     # Download the model ckpt file
-    wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O ./stable_diffusion/modelstore/sd-v1-4.ckpt
+    wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O ./input/models/sd-v1-4.ckpt
 fi
 
 #if [ ! -f "./stable_diffusion/modelstore/clip.bin" ]; then
