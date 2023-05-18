ee8b93adc57d7bb384815d18e586d6d0f8043de4
haltingstate <1774230+haltingstate@users.noreply.github.com>
2023-05-18 16:21:59 +0800

    fixing

--- stable_diffusion/inference.py
+++ stable_diffusion/inference.py
@@ -23,7 +23,23 @@ if USE_LDM:
 #copied from 
 # https://huggingface.co/spaces/multimodalart/latentdiffusion/blob/main/latent-diffusion/ldm/util.py
 #ldm
+import importlib
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    #wtf
+    print("get_obj_from_str: module= " + module)
+    print("get_obj_from_str: cls= " + cls)
+    #wtf is this doing
+    #imports = importlib.import_module(module, package=None)
+    imports = nil
+    return getattr(imports, cls)
 
+#copied from 
+# https://huggingface.co/spaces/multimodalart/latentdiffusion/blob/main/latent-diffusion/ldm/util.py
+#ldm
 def instantiate_from_config(config):
     if not "target" in config:
         if config == '__is_first_stage__':
@@ -31,6 +47,7 @@ def instantiate_from_config(config):
         elif config == "__is_unconditional__":
             return None
         raise KeyError("Expected key `target` to instantiate.")
+    print("instantiate_from_config: params= " + (config.get("params", dict()) )
     return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
 
