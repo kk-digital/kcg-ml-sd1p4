fd66b3564529a12a99e407b96bc6ad6c2ba8e73c
windowsagent <juanjphc@gmail.com>
2023-05-25 13:01:53 -0600

    Fix stable diffusion docker

---
+++ docker/README.md
@@ -0,0 +1,61 @@
+# Setting up Docker for the Stable Diffusion server
+
+## Installing Docker
+
+### Add the docker GPG key to the repository
+```bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+```
+### Add docker to apt sources
+```bash
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+```
+### Update the package database
+```bash
+sudo apt-get update
+```
+### Install docker
+```bash
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+```
+### Startup docker
+```bash
+sudo systemctl enable docker --now
+sudo systemctl status docker
+```
+
+### Install the NVIDIA toolkit (for Ubuntu)
+```bash
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list && sudo apt-get update && sudo apt-get install -y nvidia-docker2 && sudo systemctl restart docker
+```
+### Verify the NVIDIA container toolkit is working
+```bash
+sudo docker run --gpus all nvidia/cuda:11.0-base nvidia-smi
+```
+This command should display information about the current GPUs on the system
+### Add yourself to the docker group
+```bash
+sudo usermod -aG docker $USER
+```
+**You must log out and log back in to apply the changes**
+### Verify you can run docker commands
+```bash
+docker run hello-world
+```
+### Create input and output directories
+```bash
+mkdir -p input/model output
+```
+
+### Download the model
+```bash
+./download-model.sh
+```
+### Create the Docker image
+
+```bash
+export DOCKER_BUILDKIT=1
+docker build . --file ./docker/Dockerfile --tag stable-diffusion
+docker run --gpus all -v ./input:/input -v ./output:/output -v stable-diffusion
+```
+Replace /path/to/input with your input path, and /path/to/output with your output that
--- stable_diffusion/script_run_diffusion.py
+++ stable_diffusion/script_run_diffusion.py
@@ -1,33 +1,45 @@
-import os
+import os, sys
+import hashlib
 import argparse
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 import subprocess
+from text_to_image import Txt2Img
 import torch
-import numpy as np
 from tqdm import tqdm
 
-def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_images', type=int, default=10, help='Number of images to generate')
-    parser.add_argument('--batch_size', type=int, default=1, help='Batch size')
-    args = parser.parse_args()
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_images', type=int, default=10, help='Number of images to generate')
+parser.add_argument('--batch_size', type=int, default=1, help='Batch size')
+args = parser.parse_args()
 
-    # Load the diffusion model
-    model_path = '/input/model/sd-v1-4.ckpt'
+# Set the output directory
+output_dir = os.path.join(os.path.dirname(__file__), '/output/')
 
-    # Load the prompts
-    prompts_path = '/input/prompts.txt'
-    with open(prompts_path, 'r') as f:
-        prompts = f.readlines()
+# Check if CUDA is available
+if not torch.cuda.is_available():
+    print("WARNING: You are running this script without CUDA. Brace yourself for a slow ride.")
 
-    # Generate the images
-    device = torch.device('cuda')
-    batch_size = args.batch_size
-    for i in tqdm(range(0, args.num_images, batch_size)):
-        batch_prompts = prompts[i:i+batch_size]
-        cmd = ['python3', '/repo/text_to_image.py', '--checkpoint', model_path,'--prompt'] + batch_prompts
-        print(cmd)
-        subprocess.run(cmd, check=True)
+# Load the prompts
+prompts_path = '/input/prompts.txt'
+with open(prompts_path, 'r') as f:
+    prompts = f.readlines()
 
-if __name__ == '__main__':
-    main()
+# Load the Stable Diffusion model
+checkpoint_path = os.path.join(os.path.dirname(__file__), '/input/model/sd-v1-4.ckpt')
+sampler_name = 'ddim'
+txt2img = Txt2Img(checkpoint_path=checkpoint_path, sampler_name=sampler_name, n_steps=20)
+
+# Generate the images
+batch_size = args.batch_size
+for prompt in prompts:
+    hash = hashlib.sha1(prompt.encode("UTF-8")).hexdigest()
+    filename = hash[:10] + ".jpg"
+    dest_path = os.path.join(output_dir, image_name)
+    # Check if the image already exists
+    if not os.path.isfile(dest_path):
+        txt2img(dest_path=dest_path, batch_size=batch_size, prompt=prompt)
+
+# Unload model
+del txt2img
+print("done!")
