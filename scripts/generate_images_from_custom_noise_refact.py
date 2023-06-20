5954d1535e5f6bbcff69c016b5cc7ecb02cbdfff
Igor Strozzi <igor.strozzi@gmail.com>
2023-06-19 21:17:41 -0300

    developing a function to grid search on temperature

--- scripts/generate_images_from_custom_noise_refact.py
+++ scripts/generate_images_from_custom_noise_refact.py
@@ -6,12 +6,63 @@ import shutil
 from typing import Callable
 from tqdm import tqdm
 
-from stable_diffusion.utils.model import save_images, save_image_grid
+# from stable_diffusion.utils.model import save_images
 from custom_noise.text_to_image_custom import Txt2Img
 
+from typing import BinaryIO, List, Optional, Union
+from pathlib import Path
+from PIL import Image
+
+import torchvision
+import numpy as np
+
+def save_images(images: torch.Tensor, dest_path: str, img_format: str = 'jpeg'):
+    """
+    ### Save a images
+
+    :param images: is the tensor with images of shape `[batch_size, channels, height, width]`
+    :param dest_path: is the folder to save images in
+    :param img_format: is the image format
+    """
+
+    # Map images to `[0, 1]` space and clip
+    images = torch.clamp((images + 1.0) / 2.0, min=0.0, max=1.0)
+    # Transpose to `[batch_size, height, width, channels]` and convert to numpy
+    images = images.cpu()
+    images = images.permute(0, 2, 3, 1)
+    images = images.float().numpy()
+
+    # Save images
+    for i, img in enumerate(images):
+        img = Image.fromarray((255. * img).astype(np.uint8))
+        img.save(dest_path, format=img_format)
+
+def save_image_grid(
+    tensor: Union[torch.Tensor, List[torch.Tensor]],
+    fp: Union[str, Path, BinaryIO],
+    format: Optional[str] = None,
+    **kwargs,
+) -> None:
+    """
+    Save a given Tensor into an image file.
+
+    Args:
+        tensor (Tensor or list): Image to be saved. If given a mini-batch tensor,
+            saves the tensor as a grid of images by calling ``make_grid``.
+        fp (string or file object): A filename or a file object
+        format(Optional):  If omitted, the format to use is determined from the filename extension.
+            If a file object was used instead of a filename, this parameter should always be used.
+        **kwargs: Other arguments are documented in ``make_grid``.
+    """
+
+    grid = torchvision.utils.make_grid(tensor, **kwargs)
+    # Add 0.5 after unnormalizing to [0, 255] to round to the nearest integer
+    ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+    im = Image.fromarray(ndarr)
+    im.save(fp, format=format)
 
 noise_seeds = [
-    2982,
+    # 2982,
     4801,
     # 1995,
     # 3598,
@@ -42,7 +93,7 @@ if len(sys.argv) > 1:
     dist_names = list(DISTRIBUTIONS.keys())
     DIST_NAME = dist_names[dist_name_index]
     #VAR_RANGE = torch.linspace(0.6, 0.8, 10) #args here should be given as command line arguments
-    VAR_RANGE = torch.linspace(0.49, 0.54, 20) #args here should be given as command line arguments
+    VAR_RANGE = torch.linspace(0.49, 0.54, 8) #args here should be given as command line arguments
     DISTRIBUTIONS = {f'{DIST_NAME}_{var.item():.4f}': dict(loc=0.0, scale=var.item()) for var in VAR_RANGE}
 else:
     DIST_NAME = 'Normal'
@@ -50,11 +101,12 @@ else:
     DISTRIBUTIONS = {f'{DIST_NAME}_{var.item():.4f}': dict(loc=0, scale=var.item()) for var in VAR_RANGE}
 
 TEMPERATURE = 1.0 #should be cli argument
+TEMP_RANGE = torch.linspace(1, 4, 8)
 DDIM_ETA = 0.0 #should be cli argument
 OUTPUT_DIR = os.path.abspath('./output/noise-tests/')
 
 CLEAR_OUTPUT_DIR = True
-NUM_ARTISTS = 2
+NUM_ARTISTS = 1
 def get_all_torch_distributions() -> tuple[list[str], list[type]]:
     
     torch_distributions_names = torch.distributions.__all__
@@ -178,6 +230,7 @@ def generate_images_from_dist(
                         noise_fn = noise_fn,
                         temperature=temperature,
                     )
+                    print(f"temperature: {temperature}")
                     # print("img shape: ", images.shape)
                     prompt_batch.append(images)
                     # print("len prompt batch: ", len(prompt_batch))
@@ -233,12 +286,70 @@ def generate_images_from_dist_dict(
     grid = torch.cat(img_grids, dim=0)
     print("grid shape: ", grid.shape)
     save_image_grid(grid, dest_path, nrow=NUM_SEEDS, normalize=True, scale_each=True)
-  
+
+def generate_images_from_temp_range(
+        distributions: dict[str, dict[str, float]], 
+        txt2img: Txt2Img,                                      
+        output_dir: str = OUTPUT_DIR, 
+        clear_output_dir: bool = CLEAR_OUTPUT_DIR,
+        prompt_prefix: str="A woman with flowers in her hair in a courtyard, in the style of",
+        artist_file: str=os.path.abspath('./input/artists.txt'),
+        num_artists: int=1,
+        batch_size: int=1,
+        temperature_range = TEMP_RANGE,
+        ):
+    # Clear the output directory
+
+    if clear_output_dir:
+        shutil.rmtree(output_dir)
+        os.makedirs(output_dir, exist_ok=True)
+    # Create the folder structure for the outputs
+    create_folder_structure(distributions, output_dir)
+    total_images, prompts = get_all_prompts(prompt_prefix, artist_file, num_artists = 1)
+    num_distributions = len(distributions)
+    prompts = list(prompts)
+    print("num_distributions:", num_distributions)
+    noise_seed = noise_seeds[0]
+    prompt = prompts[0]
+
+    # Generate the images
+    img_grids = []
+    for distribution_index, (distribution_name, params) in enumerate(distributions.items()):
+       noise_fn = lambda shape, device = None: get_torch_distribution_from_name(DIST_NAME)(**params).sample(shape).to(device)
+       img_rows = []
+       for temperature in temperature_range:
+        images = txt2img.generate_images(
+        batch_size=batch_size,
+        prompt=prompt,
+        seed=noise_seed,
+        noise_fn = noise_fn,
+        temperature=temperature.item(),
+                    )
+        print(temperature)
+        
+        image_name = f"n{noise_seed:04d}_t{temperature}.jpg"
+        dest_path = os.path.join(os.path.join(output_dir, distribution_name), image_name)
+
+        print(f"temperature: {temperature}")
+        # print("img shape: ", images.shape)
+        img_rows.append(images)
+        # print("len prompt batch: ", len(prompt_batch))
+        save_images(images, dest_path=dest_path)
+       row = torch.cat(img_rows, dim=0)
+       img_grids.append(row) 
+    
+    dest_path = os.path.join(output_dir, f"grid_all.jpg")
+    
+    grid = torch.cat(img_grids, dim=0)
+    print("grid shape: ", grid.shape)
+    save_image_grid(grid, dest_path, nrow=len(TEMP_RANGE), normalize=True, scale_each=True)
+
       
 
 def main():
     txt2img = init_txt2img(ddim_eta=DDIM_ETA)
-    generate_images_from_dist_dict(DISTRIBUTIONS, txt2img, num_artists=NUM_ARTISTS, batch_size=1, temperature=TEMPERATURE)
+    generate_images_from_temp_range(DISTRIBUTIONS, txt2img, num_artists=NUM_ARTISTS, batch_size=1, temperature_range=TEMP_RANGE)
+    # generate_images_from_dist_dict(DISTRIBUTIONS, txt2img, num_artists=NUM_ARTISTS, batch_size=1, temperature=TEMPERATURE)
 
 if __name__ == "__main__":
     main()
