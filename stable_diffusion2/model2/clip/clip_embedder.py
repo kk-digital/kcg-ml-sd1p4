a2cc3bf8298c88ea161a2c36c412b63d5cecda56
Igor Strozzi <igor.strozzi@gmail.com>
2023-06-25 04:49:37 -0300

    reworked clip embedder class

--- stable_diffusion2/model2/clip/clip_embedder.py
+++ stable_diffusion2/model2/clip/clip_embedder.py
@@ -1,4 +1,6 @@
+
 #%%
+
 
 """
 ---
@@ -14,82 +16,84 @@ It uses HuggingFace Transformers CLIP model.
 """
 
 from typing import List
-
+import torch
 from torch import nn, save
 from os.path import join
-# from transformers import CLIPTokenizer, CLIPTextModel
-import torch
-from clip_text_model import CLIPTextModel
-from clip_tokenizer import CLIPTokenizer
-
+from transformers import CLIPTokenizer, CLIPTextModel
+from safetensors.torch import save_model, load_model, load_file
 
 class CLIPTextEmbedder(nn.Module):
     """
     ## CLIP Text Embedder
     """
 
-    def __init__(self, device="cuda:0", max_length: int = 77):
+    def __init__(self, version: str = "openai/clip-vit-large-patch14", device="cuda:0", max_length: int = 77):
         """
         :param version: is the model version
         :param device: is the device
         :param max_length: is the max length of the tokenized prompt
         """
         super().__init__()
+        self.version = version
+        self.model = None
         self.tokenizer = None
         self.transformer = None
         self.device = device
         self.max_length = max_length
+    
+    def save(self, tokenizer_path: str = "./clip_tokenizer.ckpt", transformer_path: str = "./clip_transformer.ckpt"):
+        torch.save(self.tokenizer, tokenizer_path)
+        torch.save(self.transformer, transformer_path)
+        # save_model(self.tokenizer, tokenizer_path)
+        # save_model(self.transformer, transformer_path)
+    
+    def load(self, tokenizer_path: str = "./clip_tokenizer.ckpt", transformer_path: str = "./clip_transformer.ckpt"):
+        self.tokenizer = torch.load(tokenizer_path, map_location=self.device)
+        self.transformer = torch.load(transformer_path, map_location=self.device)        
+        # self.tokenizer = load_model(tokenizer_path, map_location=self.device)
+        # self.transformer = load_model(transformer_path, map_location=self.device)
+    
+    def unload(self):        
+        del self.tokenizer
+        del self.transformer
+        torch.cuda.empty_cache()
+        self.tokenizer = None
+        self.transformer = None
 
-    def load(self, tokenizer_path: str = "./input/model/clip_tokenizer.ckpt", transformer_path: str = "./input/model/clip_transformer.ckpt"):
-        self.tokenizer = CLIPTokenizer(device=self.device, max_length=self.max_length)
-        self.tokenizer.load(tokenizer_path)
-        self.transformer = CLIPTextModel(device=self.device, max_length=self.max_length)
-        self.transformer.load(transformer_path)
-
-    def save(self, tokenizer_path: str = "./input/model/clip_tokenizer.ckpt", transformer_path: str = "./input/model/clip_transformer.ckpt", embedder_path: str = "./input/model/clip_embedder.ckpt"):
-
-        torch.save(self, embedder_path) 
-        
-    def load_from_lib(self, version: str = "openai/clip-vit-large-patch14"):
-        self.tokenizer = CLIPTokenizer(device=self.device, max_length=self.max_length)
-        self.tokenizer.load_from_lib(version = version)
-        self.transformer = CLIPTextModel(device=self.device, max_length=self.max_length)
-        self.transformer.load_from_lib(version = version)
+    def hugload(self):
+        self.tokenizer = CLIPTokenizer.from_pretrained(self.version)
+        # Load the CLIP transformer
+        self.transformer = CLIPTextModel.from_pretrained(self.version).eval().to(self.device)
 
     def forward(self, prompts: List[str]):
         """
         :param prompts: are the list of prompts to embed
         """
         # Tokenize the prompts
-        tokens = self.tokenizer(prompts, truncation=True, max_length=self.max_length, return_length=True,
+        batch_encoding = self.tokenizer(prompts, truncation=True, max_length=self.max_length, return_length=True,
                                         return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
+        # Get token ids
+        tokens = batch_encoding["input_ids"].to(self.device)
         # Get CLIP embeddings
-        return self.transformer(tokens)
-
+        return self.transformer(input_ids=tokens).last_hidden_state
 # %%
 prompts = ["", "A painting of a computer virus", "A photo of a computer virus"]
 
 clip = CLIPTextEmbedder()
 
-tok = CLIPTokenizer(); tok.load_from_lib()
-transformer = CLIPTextModel(); transformer.load_from_lib()
-# %%
-tokens = tok(prompts)
-print(tokens)
-# %%
-embeddings = transformer(tokens)
-# %%
 
 # %%
-clip.tokenizer = tok
-clip.transformer = transformer
+clip.hugload()
+embbedings1 = clip(prompts)
 #%%
+
+clip.save()  
+# %%
+clip.unload()
+# %%
+clip.load()
+# %%
 embeddings2 = clip(prompts)
 # %%
-embeddings.shape
-# %%
-embeddings2.shape
-# %%
-(embeddings == embeddings2).all()
-
+torch.allclose(embbedings1, embeddings2)
 # %%
