4c086a2f77a1e81a292ca0180b64d9ab9da06a51
windowsagent <juanjphc@gmail.com>
2023-06-25 21:36:22 -0600

    Add regularization images for training LoRa

--- lora-tuning/lora_train.py
+++ lora-tuning/lora_train.py
@@ -29,7 +29,7 @@ def check_dataset(dataset_dir):
     raise ValueError("Invalid dataset path.")
 
 def generate_config(config_file, dataset_config_file, model_file, activation_tags, max_train_epochs, save_every_n_epochs, unet_lr, text_encoder_lr, lowram,
-                    network_dim, network_alpha, batch_size, caption_extension, config_dir, log_dir, repo_dir, output_dir, accelerate_config_file,
+                    network_dim, network_alpha, batch_size, caption_extension, config_dir, log_dir, repo_dir, output_dir, accelerate_config_file, dataset_reg,
                     continue_from_lora, resolution,
                     num_repeats, dataset, project_name):
 
@@ -99,30 +99,65 @@ def generate_config(config_file, dataset_config_file, model_file, activation_tag
         f.write(toml.dumps(config_dict))
     print(f"\nConfig saved to {config_file}")
 
-    dataset_config_dict = {
-        "general": {
-            "resolution": resolution,
-            "keep_tokens": activation_tags,
-            "flip_aug": False,
-            "caption_extension": caption_extension,
-            "enable_bucket": True,
-            "bucket_reso_steps": 64,
-            "bucket_no_upscale": False,
-            "min_bucket_reso": 320 if resolution > 640 else 256,
-            "max_bucket_reso": 1280 if resolution > 640 else 1024
-        },
-        "datasets": [
-            {
-                "subsets": [
-                    {
-                        "num_repeats": num_repeats,
-                        "image_dir": dataset,
-                        "class_tokens": None if caption_extension else project_name
-                    }
-                ]
-            }
-        ]
-    }
+    if dataset_reg is None:
+        dataset_config_dict = {
+            "general": {
+                "resolution": resolution,
+                "keep_tokens": activation_tags,
+                "flip_aug": False,
+                "caption_extension": caption_extension,
+                "enable_bucket": True,
+                "bucket_reso_steps": 64,
+                "bucket_no_upscale": False,
+                "min_bucket_reso": 320 if resolution > 640 else 256,
+                "max_bucket_reso": 1280 if resolution > 640 else 1024
+            },
+            "datasets": [
+                {
+                    "subsets": [
+                        {
+                            "num_repeats": num_repeats,
+                            "image_dir": dataset,
+                            "class_tokens": None if caption_extension else project_name
+                        }
+                    ]
+                }
+            ]
+        }
+    else:
+        _, reg_name = os.path.split(dataset_reg)
+        dataset_config_dict = {
+            "general": {
+                "resolution": resolution,
+                "keep_tokens": activation_tags,
+                "flip_aug": False,
+                "caption_extension": caption_extension,
+                "enable_bucket": True,
+                "bucket_reso_steps": 64,
+                "bucket_no_upscale": False,
+                "min_bucket_reso": 320 if resolution > 640 else 256,
+                "max_bucket_reso": 1280 if resolution > 640 else 1024
+            },
+            "datasets": [
+                {
+                    "subsets": [
+                        {
+                            "num_repeats": num_repeats,
+                            "image_dir": dataset,
+                            "class_tokens": None if caption_extension else project_name
+                        },
+                        {
+                            "num_repeats": 1,
+                            "image_dir": os.path.abspath(dataset_reg),
+                            "class_tokens": reg_name,
+                            "is_reg" : True,
+                            "keep_tokens": 1
+                        }
+                    ]
+                }
+            ]
+        }
+
 
     with open(dataset_config_file, "w") as f:
         f.write(toml.dumps(dataset_config_dict))
@@ -137,6 +172,9 @@ def main(args):
 
     # Check dataset and extract if necessary
     print(f"Dataset directory: {args.dataset}")
+    if not args.dataset_reg is None:
+        print(f"Regularization dataset directory: {args.dataset_reg}")
+
 
     # Patch kohya for minor stuff
     if args.lowram:
@@ -215,6 +253,8 @@ def parse_arguments():
                         help="Path to the Lora file from which to continue training.")
     parser.add_argument("--accelerate_config_file", default=None,
                         help="Path to the accelerate distributed training configuration file.")
+    parser.add_argument("--dataset_reg", default=None, type=str,
+                        help="(Optional) Add a directory for a set of normalization images to train the LoRa. The name of the folder will be used as the class token (eg. woman for a set of images of women).")
     args = parser.parse_args()
     set_defaults(args)
     return args
@@ -244,6 +284,7 @@ def set_defaults(args):
         args.output_dir = os.path.abspath(os.path.join("./output/LoRa/", args.project_name, "output"))
 
     args.dataset = check_dataset(args.dataset)
+    args.dataset_reg = check_dataset(args.dataset_reg)
     if args.num_repeats is None:
         total_image_files = len([file for file in os.listdir(args.dataset) if file.endswith((".png", ".jpg", ".jpeg"))])
         args.num_repeats = round(300/ total_image_files)
