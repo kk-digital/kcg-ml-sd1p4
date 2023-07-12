dced11f0f1fba9646272698fc090f6019cf9abd8
Igor Strozzi <igor.strozzi@gmail.com>
2023-07-11 22:21:46 -0300

    full notebook run

--- stable_diffusion2/utils/utils.py
+++ stable_diffusion2/utils/utils.py
@@ -76,8 +76,31 @@ def save_image_grid(
     # Add 0.5 after unnormalizing to [0, 255] to round to the nearest integer
     ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
     im = Image.fromarray(ndarr)
-    im.save(fp, format=format) 
+    im.save(fp, format=format)
 
+def show_image_grid(
+    tensor: Union[torch.Tensor, List[torch.Tensor]],
+    fp: Union[str, Path, BinaryIO],
+    format: Optional[str] = None,
+    **kwargs,
+) -> None:
+    """
+    Save a given Tensor into an image file.
+
+    Args:
+        tensor (Tensor or list): Image to be saved. If given a mini-batch tensor,
+            saves the tensor as a grid of images by calling ``make_grid``.
+        fp (string or file object): A filename or a file object
+        format(Optional):  If omitted, the format to use is determined from the filename extension.
+            If a file object was used instead of a filename, this parameter should always be used.
+        **kwargs: Other arguments are documented in ``make_grid``.
+    """
+
+    grid = torchvision.utils.make_grid(tensor, **kwargs)
+    # Add 0.5 after unnormalizing to [0, 255] to round to the nearest integer
+    ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+    im = Image.fromarray(ndarr)
+    return im
     
 
 
