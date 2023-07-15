497a5e2e70ef6b91eff79a7d5ea0b15f90b7cccc
Igor Strozzi <igor.strozzi@gmail.com>
2023-07-15 14:48:03 -0300

    refactored; CLI added

--- scripts2/generate_images_from_embeddings.py
+++ scripts2/generate_images_from_embeddings.py
@@ -1,211 +1,138 @@
 import os
-
-from text_to_image import Txt2Img
+import sys
 import torch
+import shutil
 import time
+import argparse
+
+from typing import Callable
 from tqdm import tqdm
 
-from stable_diffusion2.utils.utils import save_images, save_image_grid
-from stable_diffusion2.model.vae.autoencoder import Autoencoder
-from stable_diffusion2.model.vae.encoder import Encoder
-from stable_diffusion2.model.vae.decoder import Decoder
-from stable_diffusion2.model.clip_text_embedder.clip_text_embedder import CLIPTextEmbedder
-from stable_diffusion2.model.unet.unet import UNetModel
-from cli_builder import CLI
-from constants import CHECKPOINT_PATH, EMBEDDER_PATH
 
-noise_seeds = [
+from auxiliary_functions import get_torch_distribution_from_name
+
+from text_to_image import Txt2Img
+
+from stable_diffusion2.latent_diffusion import LatentDiffusion
+from stable_diffusion2.stable_diffusion import StableDiffusion
+from stable_diffusion2.constants import CHECKPOINT_PATH, AUTOENCODER_PATH, UNET_PATH, EMBEDDER_PATH, LATENT_DIFFUSION_PATH, ENCODER_PATH, DECODER_PATH, TOKENIZER_PATH, TRANSFORMER_PATH
+from stable_diffusion2.utils.utils import SectionManager as section
+from stable_diffusion2.utils.utils import save_image_grid, save_images, check_device
+
+from os.path import join
+
+# CHECKPOINT_PATH = os.path.abspath('./input/model/v1-5-pruned-emaonly.ckpt')
+
+OUTPUT_DIR = os.path.abspath('./output/noise-tests/from_embeddings')
+EMBEDDED_PROMPTS_DIR = os.path.abspath('./input/embedded_prompts/')
+
+NOISE_SEEDS = [
     2982,
-    # 4801,
-    # 1995,
-    # 3598,
-    # 987,
-    # 3688,
-    # 8872,
-    # 762
+    4801,
+    1995,
+    3598,
+    987,
+    3688,
+    8872,
+    762
 ]
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def load_embeddings(path= os.path.abspath('./input/prompt_embeddings.pt')):
-    return torch.load(path)
+parser = argparse.ArgumentParser(
+        description='')
+
+parser.add_argument('-p', '--embedded_prompts_dir', type = str, default = EMBEDDED_PROMPTS_DIR,\
+                    help = "The path to the directory containing the embedded prompts tensors. Defaults to EMBEDDED_PROMPTS_DIR constant, which should be './input/embedded_prompts/'")
+parser.add_argument('-od', '--output_dir', type = str, default = OUTPUT_DIR, help = "The output directory. defaults to OUTPUT_DIR constant, which should be './output/noise-tests/from_embeddings'")
+parser.add_argument('--num_seeds', type=int, default=3)
+parser.add_argument('-bs', '--batch_size', type=int, default=1)
+parser.add_argument('--temperature', type=float, default=1.0)
+parser.add_argument('--ddim_eta', type=float, default=0.0)
+parser.add_argument('--clear_output_dir', type=bool, default=False)
+parser.add_argument('--cuda_device', type=str, default="cuda:0")
+
+args = parser.parse_args() 
+
+EMBEDDED_PROMPTS_DIR = args.embedded_prompts_dir
+OUTPUT_DIR = args.output_dir
+NUM_SEEDS = args.num_seeds
+BATCH_SIZE = args.batch_size
+TEMPERATURE = args.temperature
+DDIM_ETA = args.ddim_eta
+CLEAR_OUTPUT_DIR = args.clear_output_dir
+DEVICE = args.cuda_device
+
+NOISE_SEEDS = NOISE_SEEDS[:NUM_SEEDS]
 
 
+def init_stable_diffusion(device, sampler_name = "ddim", n_steps = "20", ddim_eta = 0.0):
+    
+    device = check_device(device)
+    
+    stable_diffusion = StableDiffusion(device = device, sampler_name=sampler_name, n_steps=n_steps, ddim_eta=ddim_eta)
+    
+    stable_diffusion.quick_initialize()
+    stable_diffusion.model.load_unet()
+    stable_diffusion.model.load_autoencoder().load_decoder()
+
+    return stable_diffusion
 
 def generate_images_from_embeddings(
-    embeddings_path: str='prompt_embeddings.pt',
-    output_dir: str='./output/noise-tests/',
-    checkpoint_path: str=CHECKPOINT_PATH,
+    embeddings_dir: str=EMBEDDED_PROMPTS_DIR,
+    output_dir: str=OUTPUT_DIR,
     sampler_name: str='ddim',
     n_steps: int=20,
-    batch_size: int=1,
+    batch_size: int=BATCH_SIZE,
+    noise_seeds: list[int]=NOISE_SEEDS,
+    clear_output_dir: bool=CLEAR_OUTPUT_DIR,
+    ddim_eta: float = DDIM_ETA,
+    cuda_device: str = DEVICE
 ):
 
-    embeddings = load_embeddings()
-    print(embeddings[0].shape)
+    null_cond = torch.load(join(embeddings_dir, 'null_cond.pt'), map_location=DEVICE)
+    print(null_cond.shape)
+    embeddings = torch.load(join(embeddings_dir, 'embedded_prompts.pt'), map_location=DEVICE)
 
     for embedding in embeddings:
         print(embedding.shape)
-    
-    txt2img = Txt2Img(checkpoint_path=checkpoint_path, sampler_name=sampler_name, n_steps=n_steps)
-    # clip_text_embedder = CLIPTextEmbedder(
-    #         device=DEVICE,
-    #     )
-    clip_text_embedder = None
-    t0_clip = time.time()
-    
-    
-    clip_text_embedder_model = torch.load(EMBEDDER_PATH)
-    t1_clip = time.time()
-    
-    print("Time to load CLIP from disk: %.2f seconds" % (t1_clip-t0_clip))
-    print("CLIP model: ", clip_text_embedder_model, type(clip_text_embedder_model))
-    time_before_initialization = time.time()
+    stable_diffusion = init_stable_diffusion(device=cuda_device, sampler_name=sampler_name, n_steps=n_steps, ddim_eta=ddim_eta)
+  
+    if clear_output_dir:
+        try:
+            shutil.rmtree(output_dir)
+        except Exception as e:
+            print(e)
 
-    txt2img.initialize_script(clip_text_embedder=clip_text_embedder_model)
-
-    time_after_initialization = time.time()
-    null_prompt = embeddings[0].unsqueeze(0)
-    # null_prompt = embeddings[0]
-    print(null_prompt.shape)
+    os.makedirs(output_dir, exist_ok=True)
+    
     total_images = len(noise_seeds) * len(embeddings)
+    
     with torch.no_grad():
         with tqdm(total=total_images, desc='Generating images', ) as pbar:
+            img_count = 0
+            img_grid = []
             for prompt_index, prompt in enumerate(embeddings):
-                
+                img_row = []
                 for seed_index, noise_seed in enumerate(noise_seeds):
-                    p_bar_description = f"Generating image {seed_index+prompt_index+1} of {total_images}"
+                    p_bar_description = f"Generating image {img_count+1} of {total_images}"
                     pbar.set_description(p_bar_description)
 
                     image_name = f"n{noise_seed:04d}_a{prompt_index+1:04d}.jpg"
-                    dest_path = os.path.join(output_dir, image_name)
+                    dest_path = join(output_dir, image_name)
 
-                    # images = txt2img.generate_images(
-                    #     batch_size=batch_size,
-                    #     prompt="A painting of a cat",
-                    #     seed=noise_seed
-                    # )
-                    images = txt2img.generate_images_from_embeddings(
+                    images = stable_diffusion.generate_images_from_embeddings(
                         batch_size=batch_size,
-                        embedded_prompt=prompt,
-                        null_prompt=null_prompt,
-                        seed=noise_seed
+                        embedded_prompt=prompt.unsqueeze(0),
+                        null_prompt=null_cond,
+                        seed=noise_seed,
+                        temperature=TEMPERATURE
                     )
-
+                    print(images.shape)
                     save_images(images, dest_path=dest_path)
-                    
+                    img_row.append(images)
+                    img_count += 1
                     pbar.update(1)
-
-    end_time = time.time()
-
-    show_summary(
-        total_time=end_time - time_before_initialization,
-        partial_time=end_time - time_after_initialization,
-        total_images=total_images,
-        output_dir=output_dir
-    )
-
-
-
-
-
-
-
-def generate_prompt(prompt_prefix, artist):
-    # Generate the prompt
-    prompt = f"{prompt_prefix} {artist}"
-    return prompt
-
-
-
-def init_txt2img(checkpoint_path, sampler_name, n_steps):
-    # t0_clip = time.time()
-    
-    
-    # clip_text_embedder_model = torch.load('./input/clip_embedder.pt')
-    # t1_clip = time.time()
-    
-    # print("Time to load CLIP from disk: %.2f seconds" % (t1_clip-t0_clip))
-    # print("CLIP model: ", clip_text_embedder_model, type(clip_text_embedder_model))
-    txt2img = Txt2Img(checkpoint_path=checkpoint_path, sampler_name=sampler_name, n_steps=n_steps)
-    # txt2img.initialize_script(clip_text_embedder=clip_text_embedder_model)
-    txt2img.initialize_script(clip_text_embedder=EMBEDDER_PATH)
-    # txt2img.initialize_script()
-    return txt2img
-
-def get_all_prompts(prompt_prefix, artist_file):
-    with open(artist_file, 'r') as f:
-        artists = f.readlines()
-
-    num_seeds = len(noise_seeds)
-    number_of_artists = len(artists)
-    total_images = num_seeds * number_of_artists
-
-    print(f"Artist count: {number_of_artists}")
-    print(f"Seed count: {num_seeds}")
-    print(f"Total images: {total_images}")
-    
-    artists = filter(lambda a: a, map(lambda a: a.strip(), artists))
-    prompts = map(lambda a: generate_prompt(prompt_prefix, a), artists)
-
-    return total_images, prompts
-
-def show_summary(total_time, partial_time, total_images, output_dir):
-    print("[SUMMARY]")
-    print("Total time taken: %.2f seconds" % total_time)
-    print("Partial time (without initialization): %.2f seconds" % partial_time)
-    print("Total images generated: %s" % total_images)
-    print("Images/second: %.2f" % (total_images / total_time))
-    print("Images/second (without initialization): %.2f" % (total_images / partial_time))
-
-    print("Images generated successfully at", output_dir)
-
-# main function, called when the script is run
-def generate_images(
-    prompt_prefix: str="A woman with flowers in her hair in a courtyard, in the style of",
-    artist_file: str='./input/artists.txt',
-    output_dir: str='./output/noise-tests/',
-    checkpoint_path: str=CHECKPOINT_PATH,
-    sampler_name: str='ddim',
-    n_steps: int=20,
-    batch_size: int=1,
-):
-    time_before_initialization = time.time()
-    
-    txt2img = init_txt2img(checkpoint_path, sampler_name, n_steps)
-
-    time_after_initialization = time.time()
-
-    total_images, prompts = get_all_prompts(prompt_prefix, artist_file)
-
-    with torch.no_grad():
-        with tqdm(total=total_images, desc='Generating images', ) as pbar:
-            for prompt_index, prompt in enumerate(prompts):
-                for seed_index, noise_seed in enumerate(noise_seeds):
-                    p_bar_description = f"Generating image {seed_index+prompt_index+1} of {total_images}"
-                    pbar.set_description(p_bar_description)
-
-                    image_name = f"n{noise_seed:04d}_a{prompt_index+1:04d}.jpg"
-                    dest_path = os.path.join(output_dir, image_name)
-                    
-                    images = txt2img.generate_images(
-                        batch_size=batch_size,
-                        prompt=prompt,
-                        seed=noise_seed
-                    )
-
-                    save_images(images, dest_path=dest_path)
-                    
-                    pbar.update(1)
-
-    end_time = time.time()
-
-    show_summary(
-        total_time=end_time - time_before_initialization,
-        partial_time=end_time - time_after_initialization,
-        total_images=total_images,
-        output_dir=output_dir
-    )
-
+                img_grid.append(torch.cat(img_row, dim=0))
+            save_image_grid(torch.cat(img_grid, dim=0), join(output_dir, "grid.jpg"), nrow = NUM_SEEDS)
 def main():
     # args = CLI('Generate images from noise seeds.') \
     #     .prompt_prefix() \
