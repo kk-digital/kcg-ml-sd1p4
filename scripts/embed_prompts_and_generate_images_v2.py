ff8d7c0fa368fc84a9819da9a4ba6e68487f3b99
ssss <sdevdariani9@gmail.com>
2023-07-23 09:16:43 +0400

    fix

--- scripts/embed_prompts_and_generate_images_v2.py
+++ scripts/embed_prompts_and_generate_images_v2.py
@@ -324,7 +324,7 @@ def main():
     for i in range(NUM_ITERATIONS):
         PROMPT = generate_prompt()
         embedded_prompts, null_prompt = embed_and_save_prompts(PROMPT)
-        images.append(generate_images_from_disturbed_embeddings(sd, embedded_prompts, null_prompt, batch_size = 1))
+        images = generate_images_from_disturbed_embeddings(sd, embedded_prompts, null_prompt, batch_size = 1)
 
     image_encoder = CLIPImageEncoder(device=DEVICE)
     image_encoder.load_clip_model(**pt.clip_model)
