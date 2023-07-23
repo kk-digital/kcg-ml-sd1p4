5f30e45aae60a1354d9c1aeb1af2a7fa51c10f52
ssss <sdevdariani9@gmail.com>
2023-07-23 08:07:36 +0400

    fix prompt generator

--- scripts/embed_prompts_and_generate_images_v2.py
+++ scripts/embed_prompts_and_generate_images_v2.py
@@ -335,66 +335,65 @@ def main():
     manifest_path = join(OUTPUT_DIR, "manifest.json")
     scores_path = join(OUTPUT_DIR, "scores.json")
 
-    for i in range(NUM_ITERATIONS):
-        PROMPT = generate_prompt() # Generate new prompt for each iteration
+    for i in range(NUM_ITERATIONS):  # Adjust this value according to the number of images you want
+        PROMPT = generate_prompt()
         embedded_prompts, null_prompt = embed_and_save_prompts(PROMPT)
-        
-        images = generate_images_from_disturbed_embeddings(sd, embedded_prompts, null_prompt, batch_size = 1)
-        
-        for j, (image, embedding) in enumerate(images):
-            images_tensors.append(image)
-            torch.cuda.empty_cache()
-            get_memory_status()
-            # compute hash
-            img_hash = calculate_sha256(image.squeeze())
-            pil_image = to_pil(image.squeeze())
-            # compute aesthetic score
-            prep_img = image_encoder.preprocess_input(pil_image)
-            image_features = image_encoder(prep_img)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            score = predictor(image_features.to(DEVICE).float())
-            img_file_name = f"image_{i:06d}_{j:06d}.png"
-            img_path = join(IMAGES_DIR, img_file_name)
-            pil_image.save(img_path)
-            print(f"Image saved at: {img_path}")
 
-            if SAVE_EMBEDDINGS:
-                embedding_file_name = f"embedding_{i:06d}_{j:06d}.pt"
-                embedding_path = join(FEATURES_DIR, embedding_file_name)
-                torch.save(embedding, embedding_path)
+        # Adjust generate_images_from_disturbed_embeddings to return only one image
+        image, embedding = generate_images_from_disturbed_embeddings(sd, embedded_prompts, null_prompt)
+        images_tensors.append(image)
+        torch.cuda.empty_cache()
+        get_memory_status()
+        # Compute hash
+        img_hash = calculate_sha256(image.squeeze())
+        pil_image = to_pil(image.squeeze())
+        # Compute aesthetic score
+        prep_img = image_encoder.preprocess_input(pil_image)
+        image_features = image_encoder(prep_img)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        score = predictor(image_features.to(DEVICE).float())
+        img_file_name = f"image_{i:06d}.png"
+        img_path = join(IMAGES_DIR, img_file_name)
+        pil_image.save(img_path)
+        print(f"Image saved at: {img_path}")
 
-                print(f"Embedding saved at: {embedding_path}")
-
-                manifest_i = {                     
-                                "file-name": img_file_name,
-                                "file-hash": img_hash,
-                                "file-path": img_path,
-                            }
-                manifest.append(manifest_i)
-
-                scores_i = manifest_i.copy()
-                scores_i["score"] = score.item()
-                scores.append(scores_i)
-
-                numpy_img = np.array(pil_image)
-                largest_bounding_box = get_bounding_box_details(numpy_img)
-                center_x, center_y, center_offset_x, center_offset_y, box_w, box_h = get_bounding_box_center_offset(largest_bounding_box, numpy_img.shape)
-
-                json_output_i = manifest_i.copy()
-                json_output_i["initial-prompt"] = PROMPT
-                json_output_i["score"] = score.item()
-                json_output_i["bounding_box_center_x"] = center_x
-                json_output_i["bounding_box_center_y"] = center_y
-                json_output_i["bounding_box_center_offset_x"] = center_offset_x
-                json_output_i["bounding_box_center_offset_y"] = center_offset_y
-                json_output_i["bounding_box_width"] = box_w
-                json_output_i["bounding_box_height"] = box_h
-                json_output_i["embedding-tensor"] = embedding.tolist()
-                json_output_i["clip-vector"] = image_features.tolist()
+        if SAVE_EMBEDDINGS:
+            embedding_file_name = f"embedding_{i:06d}.pt"
+            embedding_path = join(FEATURES_DIR, embedding_file_name)
+            torch.save(embedding, embedding_path)
+            print(f"Embedding saved at: {embedding_path}")
 
 
+            manifest_i = {                     
+                            "file-name": img_file_name,
+                            "file-hash": img_hash,
+                            "file-path": img_path,
+                        }
+            manifest.append(manifest_i)
 
-                json_output.append(json_output_i)
+            scores_i = manifest_i.copy()
+            scores_i["score"] = score.item()
+            scores.append(scores_i)
+
+            numpy_img = np.array(pil_image)
+            largest_bounding_box = get_bounding_box_details(numpy_img)
+            center_x, center_y, center_offset_x, center_offset_y, box_w, box_h = get_bounding_box_center_offset(largest_bounding_box, numpy_img.shape)
+
+            json_output_i = manifest_i.copy()
+            json_output_i["initial-prompt"] = PROMPT
+            json_output_i["score"] = score.item()
+            json_output_i["bounding_box_center_x"] = center_x
+            json_output_i["bounding_box_center_y"] = center_y
+            json_output_i["bounding_box_center_offset_x"] = center_offset_x
+            json_output_i["bounding_box_center_offset_y"] = center_offset_y
+            json_output_i["bounding_box_width"] = box_w
+            json_output_i["bounding_box_height"] = box_h
+            json_output_i["embedding-tensor"] = embedding.tolist()
+            json_output_i["clip-vector"] = image_features.tolist()
+
+
+
+            json_output.append(json_output_i)
 
     images_grid = torch.cat(images_tensors)
     save_image_grid(images_grid, join(IMAGES_DIR, "images_grid.png"), nrow=int(math.log(NUM_ITERATIONS, 2)), normalize=True, scale_each=True)
