38c7a926601fab9c40064ccac28388db3a8958b3
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-07-24 08:10:29 +0100

    change image size

--- scripts/text_to_image.py
+++ scripts/text_to_image.py
@@ -49,7 +49,7 @@ class Txt2Img(StableDiffusionBaseScript):
                  seed: int = 0,
                  batch_size: int = 1,
                  prompt: str,
-                 h: int = 64, w: int = 64,
+                 h: int = 512 * 4, w: int = 512 * 4,
                  uncond_scale: float = 7.5,
                  low_vram: bool = False,
                  noise_fn = torch.randn,
