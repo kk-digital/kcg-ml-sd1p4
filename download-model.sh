c84bf2a9ff3951025d2aab5c90d688538b4772c5
Igor Strozzi <istrozzi@matematica.ufrj.br>
2023-07-25 21:21:15 -0300

    removed ckpt download

--- download-model.sh
+++ download-model.sh
@@ -3,12 +3,6 @@
 # Make model directory if not there
 mkdir -p ./input/model
 
-# Check if the model ckpt file exists
-if [ ! -f "./input/model/sd-v1-4.ckpt" ]; then
-    # Download the model ckpt file
-    wget https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O ./input/model/sd-v1-4.ckpt
-fi
-
 # Check if the model file exists
 if [ ! -f "./input/model/v1-5-pruned-emaonly.safetensors" ]; then
     # Download the model file
