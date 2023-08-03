bbe7bd513c77005ad506b6b80d78133d3dc66bdf
Amaury Hernandez-Aguila <amherag@gmail.com>
2023-08-02 19:57:58 -0700

    Added percentages column to report

--- scripts/prompt_score.py
+++ scripts/prompt_score.py
@@ -89,18 +89,26 @@ def split_data(input_list, validation_ratio=0.2):
 
     return validation_list, train_list
 
-def report_residuals_histogram(residuals):
+def report_residuals_histogram(residuals, type):
     max_residual = max(residuals)
     hist_bins = np.linspace(0, max_residual, 11)
 
     histogram, bin_edges = np.histogram(residuals, bins=hist_bins)
+    total_residues = len(residuals)
 
-    histogram_string = "Residuals Histogram:\n"
+    histogram_string = f"\n{type} Residuals Histogram:\n"
+    histogram_string += f"{'Range':<13} {'Percentage':<12} {'Histogram'}\n"
+
+    max_digits = 12  # Maximum digits for percentage (including decimal point)
     for i, count in enumerate(histogram):
         bin_start, bin_end = bin_edges[i], bin_edges[i + 1]
-        histogram_string += f"{bin_start:.2f} - {bin_end:.2f}: {'*' * count}\n"
+        percentage = (count / total_residues) * 100
+        asterisks = int(percentage / 2)
+        percentage_str = f"{percentage:.2f}%"
+        histogram_string += f"{bin_start:.2f} - {bin_end:.2f}   {percentage_str:<{max_digits}} {'*' * asterisks}\n"
 
     return histogram_string
+
 
 # Custom JSON decoder for NumPy arrays
 class NumpyArrayDecoder(json.JSONDecoder):
