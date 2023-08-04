64cb751ba56985c1dc3be0bd7c0bdf8d6d4dc28e
kenje4090 <kenje4090@gmail.com>
2023-08-04 17:17:25 +0800

    Update print statements, add CLIp or Stable Diffusion

--- process_models.py
+++ process_models.py
@@ -9,34 +9,36 @@ config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolat
 config.read('config.ini')
 
 if __name__ == "__main__":
-    with section("Initializing CLIP image encoder"):
+    # CLIP
+    with section(
+            "Initialize CLIP image encoder and load submodels from lib"
+    ):
+        img_encoder = CLIPImageEncoder()
+        img_encoder.load_submodels(image_processor_path=config["MODELS_DIRS"].get('CLIP_MODEL_DIR'),
+                                   vision_model_path=config["MODELS_DIRS"].get('CLIP_MODEL_DIR'))
+    with section("CLIP: Saving image encoder submodels"):
+        img_encoder.save_submodels()
+        img_encoder.unload_submodels()
+        img_encoder.save()
+
+    # STABLE DIFFUSION
+    with section("Initializing Stable Diffusion Model"):
         model = initialize_latent_diffusion(
             path=config["STABLE_DIFFUSION_PATHS"].get('CHECKPOINT_PATH'),
             force_submodels_init=True
         )
         summary(model)
-
-    with section(
-            "initialize CLIP image encoder and load submodels from lib"
-    ):
-        img_encoder = CLIPImageEncoder()
-        img_encoder.load_submodels(image_processor_path=config["MODELS_DIRS"].get('CLIP_MODEL_DIR'),
-                                   vision_model_path=config["MODELS_DIRS"].get('CLIP_MODEL_DIR'))
-    with section("save image encoder submodels"):
-        img_encoder.save_submodels()
-        img_encoder.unload_submodels()
-        img_encoder.save()
-    with section("save vae submodels"):
+    with section("Stable Diffusion: saving vae submodels"):
         model.first_stage_model.save_submodels()  # saves autoencoder submodels (encoder, decoder) with loaded state dict
-    with section("unload vae submodels"):
+    with section("Stable Diffusion: unloading vae submodels"):
         model.first_stage_model.unload_submodels()  # unloads autoencoder submodels
-    with section("save embedder submodels"):
+    with section("Stable Diffusion: saving embedder submodels"):
         model.cond_stage_model.save_submodels()  # saves text embedder submodels (tokenizer, transformer) with loaded state dict
-    with section("unload embedder submodels"):
+    with section("Stable Diffusion: unloading embedder submodels"):
         model.cond_stage_model.unload_submodels()  # unloads text embedder submodels
-    with section("save latent diffusion submodels"):
+    with section("Stable Diffusion: saving latent diffusion submodels"):
         model.save_submodels()  # saves latent diffusion submodels (autoencoder, clip_embedder, unet) with loaded state dict and unloaded submodels (when it applies)
-    with section("unload latent diffusion submodels"):
+    with section("Stable Diffusion: unloading latent diffusion submodels"):
         model.unload_submodels()  # unloads latent diffusion submodels
-    with section("save latent diffusion model"):
+    with section("Stable Diffusion: saving latent diffusion model"):
         model.save()  # saves latent diffusion model with loaded state dict and unloaded submodels
