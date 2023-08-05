f97c5138f80b0d0122beadf881799448a917cc5e
windowsagent <juanjphc@gmail.com>
2023-08-04 19:30:43 -0600

    Fix labml logger import

--- utility/labml/internal/logger/destinations/factory.py
+++ utility/labml/internal/logger/destinations/factory.py
@@ -11,10 +11,10 @@ def create_destination() -> List[Destination]:
 
     if is_ipynb():
         if is_ipynb_pycharm():
-            from labml.internal.logger.destinations.ipynb_pycharm import IpynbPyCharmDestination
+            from utility.labml.internal.logger.destinations.ipynb_pycharm import IpynbPyCharmDestination
             return [IpynbPyCharmDestination(), ConsoleDestination(False)]
         else:
-            from labml.internal.logger.destinations.ipynb import IpynbDestination
+            from utility.labml.internal.logger.destinations.ipynb import IpynbDestination
             return [IpynbDestination(), ConsoleDestination(False)]
     else:
         return [ConsoleDestination(True)]
