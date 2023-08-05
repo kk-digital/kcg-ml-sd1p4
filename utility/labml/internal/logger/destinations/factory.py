dd1eda45077310ad4431c98939e61c191322ff60
windowsagent <juanjphc@gmail.com>
2023-08-04 19:41:14 -0600

    Change ipynb notebooks to go directly to console

--- utility/labml/internal/logger/destinations/factory.py
+++ utility/labml/internal/logger/destinations/factory.py
@@ -8,9 +8,4 @@ from utility.labml.internal.util import is_ipynb, is_ipynb_pycharm
 
 def create_destination() -> List[Destination]:
     from utility.labml.internal.logger.destinations.console import ConsoleDestination
-
-    if is_ipynb():
-        from utility.labml.internal.logger.destinations.ipynb import IpynbDestination
-        return [IpynbDestination(), ConsoleDestination(False)]
-    else:
-        return [ConsoleDestination(True)]
+    return [ConsoleDestination(True)]
