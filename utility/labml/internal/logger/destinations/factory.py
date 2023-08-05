a3b9081516d6d4fe2eaf941e209114ccd551c79e
windowsagent <juanjphc@gmail.com>
2023-08-04 19:36:07 -0600

    Add ipynb as labml destination (for google colab)

--- utility/labml/internal/logger/destinations/factory.py
+++ utility/labml/internal/logger/destinations/factory.py
@@ -10,11 +10,7 @@ def create_destination() -> List[Destination]:
     from utility.labml.internal.logger.destinations.console import ConsoleDestination
 
     if is_ipynb():
-        if is_ipynb_pycharm():
-            from utility.labml.internal.logger.destinations.ipynb_pycharm import IpynbPyCharmDestination
-            return [IpynbPyCharmDestination(), ConsoleDestination(False)]
-        else:
-            from utility.labml.internal.logger.destinations.ipynb import IpynbDestination
-            return [IpynbDestination(), ConsoleDestination(False)]
+        from utility.labml.internal.logger.destinations.ipynb import IpynbDestination
+        return [IpynbDestination(), ConsoleDestination(False)]
     else:
         return [ConsoleDestination(True)]
