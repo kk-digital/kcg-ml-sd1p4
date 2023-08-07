860d52700e1410553d73364e57ee936febc67fb5
Amaury Hernandez-Aguila <amherag@gmail.com>
2023-08-07 00:11:28 -0700

    Create subdirectories for different runs of GA

--- scripts/prompts_ga.py
+++ scripts/prompts_ga.py
@@ -174,6 +174,30 @@ def generate_images_from_embeddings(embedded_prompts_array, null_prompt):
         seed=SEED, embedded_prompt=embedded_prompt, null_prompt=null_prompt)
 '''
 
+def get_next_ga_dir(directory_path):
+    num_digits = 3  # Number of digits for subdirectory names
+    subdirectory_prefix = "ga"
+    subdirectory_pattern = subdirectory_prefix + "{:0" + str(num_digits) + "}"
+
+    existing_subdirs = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, d)) and d.startswith(subdirectory_prefix)]
+
+    if not existing_subdirs:
+        # If no subdirectories exist, return "ga000"
+        next_subdir_name = subdirectory_pattern.format(0)
+    else:
+        # Find the latest subdirectory name and extract the number part
+        latest_subdir_name = max(existing_subdirs)
+        latest_subdir_number = int(latest_subdir_name[2:])
+
+        # Increment the number and format it with the specified number of digits
+        next_subdir_number = latest_subdir_number + 1
+        next_subdir_name = subdirectory_pattern.format(next_subdir_number)
+
+    # Join the next subdirectory name with the input directory path
+    next_subdirectory_path = os.path.join(directory_path, next_subdir_name)
+
+    return next_subdirectory_path
+
 # Function to calculate the chad score for batch of images
 def calculate_chad_score(ga_instance, solution, solution_idx):
     #set seed
@@ -280,6 +304,9 @@ def prompt_embedding_vectors(sd, prompt_array):
     embedded_prompts.to("cpu")
     return embedded_prompts
 
+
+# Creating new subdirectory for this run of the GA (e.g. output/ga/images/ga001)
+os.makedirs(get_next_ga_dir(IMAGES_DIR))
 
 # Call the GA loop function with your initialized StableDiffusion model
 
