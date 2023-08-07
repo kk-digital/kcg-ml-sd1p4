84818f43324dce01372add26903081b7768dff3c
Amaury Hernandez-Aguila <amherag@gmail.com>
2023-08-07 00:09:54 -0700

    Using os.path.join to join dirs

--- scripts/prompts_ga.py
+++ scripts/prompts_ga.py
@@ -114,15 +114,15 @@ DEVICE = get_device()
 image_features_clip_model, preprocess = clip.load("ViT-L/14", device=DEVICE)
 
 #load chad score
-chad_score_model_path = 'input/model/chad_score/chad-score-v1.pth'
+chad_score_model_path = os.path.join('input', 'model', 'chad_score', 'chad-score-v1.pth')
 chad_score_predictor = ChadScorePredictor(device=DEVICE)
 chad_score_predictor.load_model(chad_score_model_path)
 
 
 #Why are you using this prompt generator?
-EMBEDDED_PROMPTS_DIR = os.path.abspath(join(base_dir, "./input/embedded_prompts/"))
+EMBEDDED_PROMPTS_DIR = os.path.abspath(join(base_dir, 'input', 'embedded_prompts'))
 
-OUTPUT_DIR = os.path.abspath(os.path.join(base_dir, "./output/ga/"))
+OUTPUT_DIR = os.path.abspath(join(base_dir, 'output', 'ga'))
 IMAGES_DIR = os.path.abspath(join(OUTPUT_DIR, "images/"))
 FEATURES_DIR = os.path.abspath(join(OUTPUT_DIR, "features/"))
 
