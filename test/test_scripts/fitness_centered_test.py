d4ddbf7280444d395b8122f7461f9ebec05b8020
ssss <sdevdariani9@gmail.com>
2023-08-14 11:43:02 +0400

    add test for bounding_box_centered

---
+++ test/test_scripts/fitness_centered_test.py
@@ -0,0 +1,11 @@
+import pytest
+from PIL import Image
+import numpy as np
+import cv2
+from ga.fitness_bounding_box_centered import centered_fitness
+
+def test_centered_fitness():
+    pil_image = Image.open("test/test_images/test_img.jpg")
+    fitness_score = centered_fitness(pil_image)
+    
+    assert 0.0 <= fitness_score <= 1.0, f"Fitness score out of bounds"
