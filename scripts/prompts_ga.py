7b9d5f21b75d52f1b0204c8bd14a321cacf2d8aa
ssss <sdevdariani9@gmail.com>
2023-08-16 11:00:55 +0400

    update prompts_ga.py

--- scripts/prompts_ga.py
+++ scripts/prompts_ga.py
@@ -75,7 +75,6 @@ from stable_diffusion.utils_backend import get_device
 from stable_diffusion.utils_image import *
 from ga.utils import get_next_ga_dir
 import ga
-from ga.prompt_generator_old import generate_prompts_old
 
 
 random.seed()
@@ -351,8 +350,16 @@ NULL_PROMPT = prompt_embedding_vectors(sd, [""])[0]
 # print("NULL_PROMPT size= ", str(torch.Tensor.size(NULL_PROMPT)))
 
 # generate prompts and get embeddings
-prompts_array = generate_prompts_old(population_size)
-embedded_prompts = prompt_embedding_vectors(sd, prompt_array=prompts_array)
+prompt_phrase_length = 10  # number of words in prompt
+prompts_array = ga.generate_prompts(population_size, prompt_phrase_length)
+
+# get prompt_str array
+prompts_str_array = []
+for prompt in prompts_array:
+    prompt_str = prompt.get_prompt_str()
+    prompts_str_array.append(prompt_str)
+
+embedded_prompts = prompt_embedding_vectors(sd, prompt_array=prompts_str_array)
 
 print("genetic_algorithm_loop: population_size= ", population_size)
 
