e9ce45b98b2b7015039c3f017355f2ec4435b41c
ssss <sdevdariani9@gmail.com>
2023-08-17 09:50:33 +0400

    update script

--- scripts/ga_bounding_box_size.py
+++ scripts/ga_bounding_box_size.py
@@ -249,7 +249,7 @@ prompts_array = ga.generate_prompts(population_size, prompt_phrase_length)
 
 # get prompt_str array
 prompts_str_array = []
-prefix_prompt = " 'centered', 'no background' , 'white background' "
+prefix_prompt = " centered, no background , white background "
 for prompt in prompts_array:
     prompt_str = prefix_prompt + prompt.get_prompt_str()
     prompts_str_array.append(prompt_str)
