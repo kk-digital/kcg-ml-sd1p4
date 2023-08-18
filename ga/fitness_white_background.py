7863f5c69f076d18e11cbaf2e51f0c9bff5b7120
ssss <sdevdariani9@gmail.com>
2023-08-18 14:05:22 +0400

    update script

--- ga/fitness_white_background.py
+++ ga/fitness_white_background.py
@@ -19,45 +19,30 @@ def white_background_fitness(pil_image):
 
     # Convert the PIL image to an OpenCV image format
     image = np.array(pil_image)[:, :, ::-1].copy()  # Convert RGB to BGR
-    
-    # Define the central region
-    h, w, _ = image.shape
-    quarter_h, quarter_w = h // 4, w // 4
-    central_region = image[quarter_h:3*quarter_h, quarter_w:3*quarter_w]
 
-    # Create a border mask and extract the border region
-    border_mask = np.ones((h, w), dtype=np.uint8)
-    border_mask[quarter_h:3*quarter_h, quarter_w:3*quarter_w] = 0
-    border_region = cv2.bitwise_and(image, image, mask=border_mask)
-
-    # Adaptive thresholding
+    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    adaptive_thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    central_thresh = adaptive_thresh[quarter_h:3*quarter_h, quarter_w:3*quarter_w]
-    border_thresh = cv2.bitwise_and(adaptive_thresh, adaptive_thresh, mask=border_mask)
 
-    # Compute contour areas
-    center_contours, _ = cv2.findContours(central_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    border_contours, _ = cv2.findContours(border_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    center_area = sum(cv2.contourArea(cnt) for cnt in center_contours)
-    border_area = sum(cv2.contourArea(cnt) for cnt in border_contours)
+    # Define the central region
+    h, w = gray.shape
+    quarter_h, quarter_w = h // 4, w // 4
+    central_region = gray[quarter_h:3*quarter_h, quarter_w:3*quarter_w]
 
-    # Check color differences
-    mean_center_color = np.mean(central_region, axis=(0, 1))
-    mean_border_color = np.mean(border_region, axis=(0, 1))
-    color_diff = np.linalg.norm(mean_center_color - mean_border_color)
-
-    # Logic for fitness score computation
-    # If there's more content in the border region compared to the center, penalize it
-    if border_area > center_area:
+    # Mask the central region out to compute the mean intensity of the border region
+    border_mask = np.ones_like(gray)
+    border_mask[quarter_h:3*quarter_h, quarter_w:3*quarter_w] = 0
+    border_pixels = gray[border_mask == 1]
+    
+    # Compute the mean pixel intensity of the border
+    mean_border_intensity = np.mean(border_pixels)
+    mean_center_intensity = np.mean(central_region)
+    
+    # Ensure the central region is not too bright compared to the border
+    if mean_center_intensity > (mean_border_intensity - 10):  # Adjust the value '20' as needed
         return 0.0
 
-    # If the color difference is below a threshold, penalize it
-    if color_diff < 20:  # This threshold can be adjusted
-        return 0.0
-
-    # Otherwise, base the score on how white the border region is
-    fitness_score = 1 - abs(np.mean(mean_border_color) - 255) / 255
+    # Compute the fitness based on the mean intensity of the border (it should be white or near-white)
+    fitness_score = 1 - abs(mean_border_intensity - 255) / 255
 
     assert 0.0 <= fitness_score <= 1.0, "Background fitness value out of bounds!"
     return fitness_score
