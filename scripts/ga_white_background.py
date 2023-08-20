de064ed0b2ae6c03b068e1861da6f0cbd89217d2
ssss <sdevdariani9@gmail.com>
2023-08-20 09:41:57 +0400

    update script

--- scripts/ga_white_background.py
+++ scripts/ga_white_background.py
@@ -216,7 +216,7 @@ def prompt_embedding_vectors(sd, prompt_array):
 
 
 generations = args.generations
-population_size = 300
+population_size = 200
 mutation_percent_genes = args.mutation_percent_genes
 mutation_probability = args.mutation_probability
 keep_elitism = args.keep_elitism
