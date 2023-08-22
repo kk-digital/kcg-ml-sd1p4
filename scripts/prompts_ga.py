452b50034a81716572752cefe356e2f280f0edc8
ssss <sdevdariani9@gmail.com>
2023-08-22 12:10:11 +0400

    change threshold value

--- scripts/prompts_ga.py
+++ scripts/prompts_ga.py
@@ -436,9 +436,9 @@ num_clip_calculations = len(ga_instance.population) * args.generations
 clip_calculations_per_second = num_clip_calculations / clip_total_time
 
 # Print the results
-log_to_file("Number of Clip Calculations: {num_clip_calculations} ")
-log_to_file("Total Time for Clip Calculations: {clip_total_time} seconds")
-log_to_file("Clip Calculations per Second {clip_calculations_per_second} ")
+log_to_file(f"Number of Clip Calculations: {num_clip_calculations} ")
+log_to_file(f"Total Time for Clip Calculations: {clip_total_time} seconds")
+log_to_file(f"Clip Calculations per Second {clip_calculations_per_second} ")
 
 
 '''
