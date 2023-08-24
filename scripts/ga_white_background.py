d196b601b6e55f8da7ff1df4405981474fe583e3
ssss <sdevdariani9@gmail.com>
2023-08-24 19:22:23 +0400

    update script

--- scripts/ga_white_background.py
+++ scripts/ga_white_background.py
@@ -285,7 +285,7 @@ prompts_array = ga.generate_prompts(population_size, prompt_phrase_length)
 
 # get prompt_str array
 prompts_str_array = []
-prefix_prompt = "green wall"
+prefix_prompt = "white robot on green background"
 # Append prefix_prompt to prompts_str_array
 prompts_str_array.append(prefix_prompt)
 
