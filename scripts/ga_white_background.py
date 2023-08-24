8d7e87bb551eb54c228260d7b0c80153cb3472a8
ssss <sdevdariani9@gmail.com>
2023-08-24 14:36:55 +0400

    update script

--- scripts/ga_white_background.py
+++ scripts/ga_white_background.py
@@ -285,7 +285,7 @@ prompts_array = ga.generate_prompts(population_size, prompt_phrase_length)
 
 # get prompt_str array
 prompts_str_array = []
-prefix_prompt = "black ball on white background,centered, isolated on white background "
+prefix_prompt = "one black ball on white background,centered,logo, black and white, isolated on white background "
 # Append prefix_prompt to prompts_str_array
 prompts_str_array.append(prefix_prompt)
 
