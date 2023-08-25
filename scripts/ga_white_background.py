6e496f5aa1e5bf5d246daf379653347e9171950d
ssss <sdevdariani9@gmail.com>
2023-08-25 08:38:37 +0400

    update script

--- scripts/ga_white_background.py
+++ scripts/ga_white_background.py
@@ -31,8 +31,8 @@ random.seed()
 N_STEPS = 20  # 20, 12
 CFG_STRENGTH = 9
 
-FIXED_SEED = False
-CONVERT_GREY_SCALE_FOR_SCORING = True
+FIXED_SEED = True
+CONVERT_GREY_SCALE_FOR_SCORING = False
 
 # Add argparse arguments
 parser = argparse.ArgumentParser(description="Run genetic algorithm with specified parameters.")
