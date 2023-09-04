c6bf05dac8fddbf878867dc2f206434ab310a123
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-04 10:23:11 +0100

    affine_combination_embeddings : chad_score model loading, text2image class

--- scripts/affine_combination_embeddings.py
+++ scripts/affine_combination_embeddings.py
@@ -5,6 +5,7 @@ import os
 import sys
 import numpy as np
 import torch
+import time
 
 base_dir = "./"
 sys.path.insert(0, base_dir)
@@ -13,6 +14,8 @@ sys.path.insert(0, os.getcwd())
 from stable_diffusion.utils_backend import get_device
 from ga.prompt_generator import generate_prompts
 from stable_diffusion.model.clip_text_embedder import CLIPTextEmbedder
+from stable_diffusion_base_script import StableDiffusionBaseScript
+from stable_diffusion.utils_backend import get_autocast, set_seed
 
 def parse_arguments():
     """Command-line arguments"""
@@ -25,14 +28,73 @@ def parse_arguments():
     parser.add_argument('--cfg_strength', type=float, default=12)
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--sampler', type=str, default='ddim')
+    parser.add_argument('--num_prompts', type=int, default=12)
+    parser.add_argument('--num_phrases', type=int, default=12)
 
     return parser.parse_args()
 
 
+
+class Txt2Img(StableDiffusionBaseScript):
+    """
+    ### Text to image class
+    """
+
+    @torch.no_grad()
+    def generate_images_latent_from_embeddings(self, *,
+                                        seed: int = 0,
+                                        batch_size: int = 1,
+                                        embedded_prompt: torch.Tensor,
+                                        null_prompt: torch.Tensor,
+                                        h: int = 512, w: int = 512,
+                                        uncond_scale: float = 7.5,
+                                        low_vram: bool = False,
+                                        noise_fn=torch.randn,
+                                        temperature: float = 1.0,
+                                        ):
+        """
+        :param seed: the seed to use when generating the images
+        :param dest_path: is the path to store the generated images
+        :param batch_size: is the number of images to generate in a batch
+        :param prompt: is the prompt to generate images with
+        :param h: is the height of the image
+        :param w: is the width of the image
+        :param uncond_scale: is the unconditional guidance scale $s$. This is used for
+            $\epsilon_\theta(x_t, c) = s\epsilon_\text{cond}(x_t, c) + (s - 1)\epsilon_\text{cond}(x_t, c_u)$
+        :param low_vram: whether to limit VRAM usage
+        """
+        # Number of channels in the image
+        c = 4
+        # Image to latent space resolution reduction
+        f = 8
+
+        if seed == 0:
+            seed = time.time_ns() % 2 ** 32
+
+        set_seed(seed)
+        # Adjust batch size based on VRAM availability
+        if low_vram:
+            batch_size = 1
+
+        # AMP auto casting
+        autocast = get_autocast()
+        with autocast:
+
+            # [Sample in the latent space](../sampler/index.html).
+            # `x` will be of shape `[batch_size, c, h / f, w / f]`
+            x = self.sampler.sample(cond=embedded_prompt,
+                                    shape=[batch_size, c, h // f, w // f],
+                                    uncond_scale=uncond_scale,
+                                    uncond_cond=null_prompt,
+                                    noise_fn=noise_fn,
+                                    temperature=temperature)
+
+            return x
+
 def combine_embeddings(embeddings_array, weight_array):
 
     # empty embedding filled with zeroes
-    result_embedding = np.zeros((77, 786))
+    result_embedding = np.zeros((1, 77, 768))
 
     num_elements = len(embeddings_array)
     for i in range(num_elements):
@@ -59,6 +121,24 @@ if __name__ == "__main__":
     clip_text_embedder = CLIPTextEmbedder(device=get_device())
     clip_text_embedder.load_submodels()
 
+    # Load default chad model
+    # hard coded for now
+    chad_score_model_path = "input/model/chad_score/chad-score-v1.pth"
+    chad_score_model_name = os.path.basename(chad_score_model_path)
+    chad_score_predictor = ChadScorePredictor(device=device)
+    chad_score_predictor.load_model(chad_score_model_path)
+
+    # Starts the text2img
+    txt2img = Txt2Img(
+        sampler_name=sampler,
+        n_steps=steps,
+        force_cpu=force_cpu,
+        cuda_device=device,
+    )
+    txt2img.initialize_latent_diffusion(autoencoder=None, clip_text_embedder=None, unet_model=None,
+                                        path=checkpoint_path, force_submodels_init=True)
+
+
     # Generate N Prompts
     prompt_list = generate_prompts(num_prompts, num_phrases)
 
@@ -67,7 +147,7 @@ if __name__ == "__main__":
     for prompt in prompt_list:
         # get the embedding from positive text prompt
         embedded_prompts = clip_text_embedder(prompt.positive_prompt_str)
-        embedded_prompts_numpy = embedded_prompts.cpu().numpy()
+        embedded_prompts_numpy = embedded_prompts.detach().cpu().numpy()
 
         del embedded_prompts
         torch.cuda.empty_cache()
@@ -77,6 +157,16 @@ if __name__ == "__main__":
     # array of random weights
     weight_array = np.random.rand(num_prompts)
     # Combinate into one Embedding
-    embedding = combine_embeddings(embedded_prompts_array, weight_array)
+    embedding_numpy = combine_embeddings(embedded_prompts_array, weight_array)
     # Maximize fitness
-    print(embedding)
+
+    embedding_vector = clip_text_embedder(embedding_numpy)
+    latent = txt2img.generate_images_latent_from_embeddings(
+                    batch_size=batch_size,
+                    embedded_prompt=cond,
+                    null_prompt=un_cond,
+                    uncond_scale=cfg_strength,
+                    seed=this_seed,
+                    w=image_width,
+                    h=image_height
+                )
