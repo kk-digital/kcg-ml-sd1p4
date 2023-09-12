813c6d0b28581a3ea384690f22fea473cf01f3d0
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-12 11:52:41 +0100

    minor fix

--- model/util_clip.py
+++ model/util_clip.py
@@ -28,6 +28,6 @@ class UtilClip():
         return image_features.to(self.device)
 
     def get_text_features(self, text):
-        tokens = clip.tokenize(text)
+        tokens = clip.tokenize(text).to(device=self.device)
         text_features = self.model.encode_text(tokens).to(device=self.device, dtype=torch.float32)
         return text_features
