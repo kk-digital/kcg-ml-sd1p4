1965610df6a8b195e1d574d8febda82bfd552046
ssss <sdevdariani9@gmail.com>
2023-09-15 15:46:14 +0400

    add print statement

--- scripts/ga_1024_embeddings.py
+++ scripts/ga_1024_embeddings.py
@@ -311,11 +311,10 @@ for prompt in prompts_array:
     prompt_str = prompt.get_positive_prompt_str()
     prompts_str_array.append(prompt_str)
 
-print(prompts_str_array)
+print("Generated Prompt Strings: ", prompts_str_array)
 
 embedded_prompts_numpy = np.array(clip_text_get_prompt_embedding_numpy(config, prompts_str_array))
-file_path = os.path.join(IMAGES_ROOT_DIR, 'embedded_prompts_numpy.npy')
-np.save(file_path, embedded_prompts_numpy)
+
 
 
 # random_mutation_min_val=5,
