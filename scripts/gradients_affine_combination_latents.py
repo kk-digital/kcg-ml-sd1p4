693f2cbfb97ac44448cffd1b0b7c9b03f8e0a12c
Mahdi <74624149+zoubirabde9a@users.noreply.github.com>
2023-09-18 09:27:45 +0100

    fixing minor compilation bug

--- scripts/gradients_affine_combination_latents.py
+++ scripts/gradients_affine_combination_latents.py
@@ -25,7 +25,7 @@ from stable_diffusion.model.clip_text_embedder import CLIPTextEmbedder
 from stable_diffusion_base_script import StableDiffusionBaseScript
 from stable_diffusion.utils_backend import get_autocast, set_seed
 from chad_score.chad_score import ChadScorePredictor
-from model.util_clip import ClipModelHuggingface
+from model.util_clip import ClipOpenAi
 from stable_diffusion.utils_image import save_images
 
 def parse_arguments():
@@ -308,7 +308,7 @@ if __name__ == "__main__":
     chad_score_predictor.load_model(chad_score_model_path)
 
     # Load the clip model
-    util_clip = ClipModelHuggingface(device=device)
+    util_clip = ClipOpenAi(device=device)
     util_clip.load_model()
 
     prompt_list = read_prompts_from_zip(prompts_path, num_prompts)
