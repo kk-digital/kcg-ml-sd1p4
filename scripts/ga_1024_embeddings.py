9f5d4b122fb928bf1abe07f0b73d0d751bd9e5b8
ssss <sdevdariani9@gmail.com>
2023-09-22 09:26:58 +0400

    reduce num genes size

--- scripts/ga_1024_embeddings.py
+++ scripts/ga_1024_embeddings.py
@@ -310,7 +310,7 @@ torch.save(NULL_PROMPT, os.path.join(FEATURES_DIR, 'null_cond.pt'))
 # print("NULL_PROMPT size= ", str(torch.Tensor.size(NULL_PROMPT)))
 
 # generate prompts and get embeddings
-num_genes = 500  # Each individual is 1024 floats
+num_genes = 3 # Each individual is 1024 floats
 prompt_phrase_length = 6  # number of words in prompt
 prompts_array = ga.generate_prompts(num_genes, prompt_phrase_length)
 
